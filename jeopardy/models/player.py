8c1dca8fee95e6cf4b6b091de2e0100c0edd0bc3
lekha <305097+lekha@users.noreply.github.com>
2020-06-21 10:41:59 -0400

    Create first migration file

--- jeopardy/models/player.py
+++ jeopardy/models/player.py
@@ -6,5 +6,8 @@ from jeopardy.models.base import BaseModel
 class Player(BaseModel):
     name = fields.CharField(255)
 
+    class Meta:
+        table = "players"
+
     def __str__(self):
         return f"Player({self.id}, {self.name})"
