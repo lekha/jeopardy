991f0b5effdab0fcb4c8bb7f6fc83fa986153ad5
lekha <305097+lekha@users.noreply.github.com>
2020-06-25 21:55:20 -0400

    Create tile component

---
+++ frontend/src/components/Tile.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="tile">
+  <p class="points">${{ points }}<p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Tile",
+  props: {
+    points: Number,
+    answer: String,
+    question: String
+  },
+  data() {
+    return {
+      points: 0,
+      answer: "Blank answer",
+      question: "Blank question"
+    }
+  }
+}
+</script>
