9ebf1106deee0635be8263dc86197a4cf61606d6
lekha <305097+lekha@users.noreply.github.com>
2020-06-27 16:16:20 -0400

    Standardize use of double-quotes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,14 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Buzzer from '@/components/buzzer.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import Buzzer from "@/components/buzzer.vue";
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
-            path: '/',
-            name: 'Buzzer',
+            path: "/",
+            name: "Buzzer",
             component: Buzzer
         }
     ]
