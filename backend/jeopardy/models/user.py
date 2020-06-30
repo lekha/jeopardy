3de0eda3fbcf28bedf3474aee56de12bacad73fe
lekha <305097+lekha@users.noreply.github.com>
2020-06-29 22:30:50 -0400

    Add pydantic models for user orm models

--- backend/jeopardy/models/user.py
+++ backend/jeopardy/models/user.py
@@ -1,5 +1,8 @@
 from enum import Enum
+from typing import Optional
 
+from pydantic import BaseModel as PydanticOrmModel
+from pydantic import constr
 from tortoise import fields
 
 from jeopardy.models.base import BaseModel
@@ -10,7 +13,11 @@ class AuthProvider(Enum):
     GOOGLE = "google"
 
 
-class AnonymousUserMetadata(BaseModel):
+class AnonymousUserMetadata(PydanticOrmModel):
+    expire_ts: datetime
+
+
+class AnonymousUserMetadataOrm(BaseModel):
     expire_ts = fields.DatetimeField()
 
     class Meta:
@@ -20,7 +27,18 @@ class AnonymousUserMetadata(BaseModel):
         return f"AnonymousUserMetadata({self.id})"
 
 
-class GoogleUserMetadata(BaseModel):
+class GoogleUserMetadata(PydanticOrmModel):
+    subject: constr(max_length=255)
+    email: constr(max_length=255)
+    given_name: Optional[constr(max_length=255)] = None
+    issuer: constr(max_length=255)
+    family_name: Optional[constr(max_length=255)] = None
+    name: Optional[constr(max_length=255)] = None
+    locale: Optional[constr(max_length=255)] = None
+    picture: Optional[constr(max_length=255)] = None
+
+
+class GoogleUserMetadataOrm(BaseModel):
     subject = fields.CharField(255, unique=True)
     email = fields.CharField(255)
     given_name = fields.CharField(255, null=True)
@@ -37,18 +55,26 @@ class GoogleUserMetadata(BaseModel):
         return f"GoogleUserMetadata({self.id}, {self.email})"
 
 
-class User(BaseModel):
+class User(PydanticOrmModel):
+    display_name: constr(max_length=255)
+    is_active: bool = True
+    auth_provider: AuthProvider
+    anonymous_metadata: Optional[AnonymousUserMetadata] = None
+    google_metadata: Optional[GoogleUserMetadata] = None
+
+
+class UserOrm(BaseModel):
     display_name = fields.CharField(255)
     is_active = fields.BooleanField(default=1)
     auth_provider = fields.CharEnumField(AuthProvider)
     anonymous_metadata = fields.ForeignKeyField(
-        "models.AnonymousUserMetadata",
+        "models.AnonymousUserMetadataOrm",
         related_name="user",
         on_delete="RESTRICT",
         null=True,
     )
     google_metadata = fields.ForeignKeyField(
-        "models.GoogleUserMetadata",
+        "models.GoogleUserMetadataOrm",
         related_name="user",
         on_delete="RESTRICT",
         null=True,
