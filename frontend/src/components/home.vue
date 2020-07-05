81fdded2b793bfa907a04b809ed36a92f196d65a
lekha <305097+lekha@users.noreply.github.com>
2020-07-05 11:11:39 -0400

    Add button to log in with google

---
+++ frontend/src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="home">
+  <a href="/login">Log In with Google</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+    }
+  }
+}
+</script>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,13 +1,20 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Buzzer from "@/components/buzzer.vue";
+import Home from "@/components/home.vue";
 
 Vue.use(Router);
 
 export default new Router({
+    mode: "history",
     routes: [
         {
             path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/buzzer",
             name: "Buzzer",
             component: Buzzer
         }
